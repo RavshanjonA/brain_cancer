b645b3f88f858e5892973697a78244da01d7c86a
Ahmadjonov-98 <akhmadjonovr.98@gmail.com>
2023-06-01 11:02:26 +0500

    change cors domain

--- config/settings.py
+++ config/settings.py
@@ -63,9 +63,7 @@ SWAGGER_SETTINGS = {
 CORS_ORIGIN_ALLOW_ALL = True
 
 CORS_ALLOWED_ORIGINS = [
-    "*vercel.com",
-    "https://sub.example.com",
-    "http://localhost:8080",
+    "*",
     "http://127.0.0.1:8000",
 ]
 
